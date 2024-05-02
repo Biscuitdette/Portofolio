08b7ed308c8f5d2406c0c5705e6919c7715417fe
Biscuitdette <120401315+Biscuitdette@users.noreply.github.com>
2024-05-02 21:49:09 +0200

    Add files via upload

---
+++ js/testimonials.js
@@ -0,0 +1,26 @@
+const testimonials = [
+    {
+        quote: "Skibidi",
+        avatar: "default",
+        name: "Skibidi",
+        title: "Skibidi"
+    },
+    {
+        quote: "Skibidi",
+        avatar: "default",
+        name: "Skibidi",
+        title: "Skibidi"
+    },
+    {
+        quote: "Skibidi",
+        avatar: "default",
+        name: "Skibidi",
+        title: "Skibidi"
+    },
+    {
+        quote: "Skibidi",
+        avatar: "default",
+        name: "Skibidi",
+        title: "Skibidi"
+    },
+]
